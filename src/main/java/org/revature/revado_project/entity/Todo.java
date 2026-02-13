2f7ba4a6a8725e51722031a5a2f8c416872796b4
ingquimbay <quimbayc@javeriana.edu.co>
2026-02-13 14:31:58 -0600

    Created user and todo entities and project packages

---
+++ src/main/java/org/revature/revado_project/entity/Todo.java
@@ -0,0 +1,45 @@
+package org.revature.revado_project.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	private String description;
+
+	@Column(nullable = false)
+	private boolean isCompleted = false;
+
+	// The Parent: Many subtasks point to one parent
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_id")
+	private Todo parent;
+
+	// The Children: One task can have many subtasks
+	@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Todo> subtasks = new ArrayList<>();
+
+}
---
+++ src/main/java/org/revature/revado_project/entity/User.java
@@ -0,0 +1,33 @@
+package org.revature.revado_project.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Todo> todos = new ArrayList<>();
+
+}
