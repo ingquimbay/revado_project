1186a89d8d60be5cd08d4c5dc8952573b96473c0
ingquimbay <quimbayc@javeriana.edu.co>
2026-02-13 13:50:39 -0600

    Initial Commit - project creation

---
+++ src/main/java/org/revature/revado_project/RevadoProjectApplication.java
@@ -0,0 +1,13 @@
+package org.revature.revado_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevadoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevadoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/revature/revado_project/RevadoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.revature.revado_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevadoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
